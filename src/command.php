d010a8f9bd85abfa303ae0b26cb026def8734be3
Robert Wagner <rw@20sec.net>
2021-03-30 21:09:07 +0200

    trialanderror

--- src/command.php
+++ src/command.php
@@ -3,7 +3,10 @@ $cmd = $argv[1];
 $slft_lib_base = __DIR__;
 
 if ($cmd == 'dev') {
-    `open http://localhost:1199/`;
+    // this wont work :)
+    // `(sleep 1 ; open http://localhost:1199/ )&`;
+    // this works!
+    shell_exec('(sleep 1 ; open http://localhost:1199/ ) 2>/dev/null >/dev/null &');
     $command = "php -S localhost:1199 -t src/ {$slft_lib_base}/dev.php";
     print "\n\n";
     print "     ***********************************\n";
