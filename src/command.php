e620a544736e000ab67e1186da3e3f678b7d906e
Robert Wagner <rw@20sec.net>
2021-03-30 17:54:34 +0200

    trialanderror

--- src/command.php
+++ src/command.php
@@ -4,7 +4,9 @@ $slft_lib_base = __DIR__;
 
 if ($cmd == 'dev') {
     $command = "php -S localhost:1199 -t src/ {$slft_lib_base}/dev.php";
-    print "starting development server $command\n";
+    print "*** slowfoot ***\n";
+    print "* starting development server $command *\n";
+    print "*****************\n";
     `$command`;
 }
 if ($cmd == 'build') {
