be4526cda4d80869655a68b784317d9d1b82ee84
Robert Wagner <rw@20sec.net>
2022-01-14 19:56:19 +0100

    simple refetch

--- src/command.php
+++ src/command.php
@@ -22,7 +22,7 @@ DOC;
 //require_once(__DIR__.'/../vendor/autoload.php');
 
 $parsed = Docopt::handle($doc, array('version'=>'slowfoot 0.1'));
-var_dump($parsed);
+#var_dump($parsed);
 $args = $parsed->args;
 
 // https://www.kammerl.de/ascii/AsciiSignature.php rounded
@@ -41,7 +41,7 @@ if ($args['dev']) {
     print $logo."\n";
 
     $FETCH = $args['-f'];
-    
+
     // evtl. fetching data
     require __DIR__ . '/boot.php';
 
