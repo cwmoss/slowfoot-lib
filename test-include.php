8f07fd054f5f677f6a21a7f8bb30522e968a3df1
rw <rw@20sec.net>
2022-01-17 00:40:49 +0100

    notiz

--- test-include.php
+++ test-include.php
@@ -11,3 +11,11 @@ require_once __DIR__ . '/src/template.php';
 <h1>hello <?=$title?> <?=$name?></h1>
 
 <p>now is <?=date("Y-m-d H:i:s")?></p>
+<?php
+/*
+
+https://images.metmuseum.org/CRDImages/ep/original/DP-20101-001.jpg
+https://www.metmuseum.org/about-the-met/policies-and-documents/image-resources
+http://data.mumok.at/data.json
+
+*/
