a4da99ab62cbec558761164ac477b24a82e8a4de
Robert Wagner <rw@20sec.net>
2022-03-03 19:35:05 +0100

    deploy

--- src/webdeploy.php
+++ src/webdeploy.php
@@ -19,28 +19,34 @@ $orig = $_SERVER['HTTP_ORIGIN'];
 header('Access-Control-Allow-Origin: '.$orig);
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Max-Age: 1000');
-if(array_key_exists('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', $_SERVER)) {
-  header('Access-Control-Allow-Headers: '
+if (array_key_exists('HTTP_ACCESS_CONTROL_REQUEST_HEADERS', $_SERVER)) {
+    header('Access-Control-Allow-Headers: '
          . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
 } else {
-//   header('Access-Control-Allow-Headers: *');
+    //   header('Access-Control-Allow-Headers: *');
 }
 
 header('Access-Control-Allow-Credentials: true');
 #  header('Access-Control-Allow-Headers: Authorization');
 header('Access-Control-Expose-Headers: Authorization');
 
-if("OPTIONS" == $_SERVER['REQUEST_METHOD']) {
-  exit(0);
+if ("OPTIONS" == $_SERVER['REQUEST_METHOD']) {
+    exit(0);
 }
 
 $ok = check_referer($_SERVER);
-if(!$ok){
-  print "\nfailed\n";
-  print_r($_SERVER);
-  exit;
+if (!$ok) {
+    print "\nfailed\n";
+    print_r($_SERVER);
+    exit;
 }
 
+$ok = check_token(getenv('SLFT_BUILD_KEY'));
+if (!$ok) {
+    print "\nauth failed\n";
+    print_r($_SERVER);
+    exit;
+}
 
 
 
@@ -50,38 +56,55 @@ if(!$ok){
 // print $cmd;
 $cmd = SLOWFOOT_BASE.'/slowfoot build';
 $converter = new AnsiToHtmlConverter();
-print $converter->convert("hier \033[1mfett\033[0m text");
-
+#print $converter->convert("hier \033[1mfett\033[0m text");
+$converter=null;
 $result = liveExecuteCommand($cmd, true, $converter);
 
-if($result['exit_status'] === 0){
-   // do something if command execution succeeds
-   print "\nok\n";
-   #`cd $dir; rsync -avz dist/ ../htdocs/`;
+if ($result['exit_status'] === 0) {
+    // do something if command execution succeeds
+    print "\nok\n";
+#`cd $dir; rsync -avz dist/ ../htdocs/`;
 } else {
     // do something on failure
-	print "\nfailed\n";
+    print "\nfailed\n";
 }
 
-function check_referer($headers){
-  $allowed = ['localhost', 'sf-photog.sanity.studio', 'kurparkverlag-gs-studio.netlify.app', 'kurparkverlag.sanity.studio'];
+function check_referer($headers)
+{
+    // local (dev) installation?
+    if ($headers['HTTP_HOST']=='localhost') {
+        return true;
+    }
 
-  # sometimes referer doesn't include the full url (/dashboard)
-  # if(!preg_match("!/dashboard$!", $headers['HTTP_REFERER'])) return false;
+    if (!isset($headers['SLFT_WEBDEPLOY_ALLOWED_HOSTS'])) {
+        return true;
+    }
+    
+    // $allowed = ['localhost', 'sf-photog.sanity.studio', 'kurparkverlag-gs-studio.netlify.app', 'kurparkverlag.sanity.studio'];
+    $allowed = explode(" ", $headers['SLFT_WEBDEPLOY_ALLOWED_HOSTS']);
 
-  $remote = parse_url($headers['HTTP_ORIGIN'], PHP_URL_HOST);
+    # sometimes referer doesn't include the full url (/dashboard)
+    # if(!preg_match("!/dashboard$!", $headers['HTTP_REFERER'])) return false;
 
-  return in_array($remote, $allowed);
+    $remote = parse_url($headers['HTTP_ORIGIN'], PHP_URL_HOST);
+
+    return in_array($remote, $allowed);
+}
+
+function check_token($token)
+{
+    $hdrs = getallheaders();
+    $hdrs = array_change_key_case($hdrs);
+    return $hdrs['x-deploy']==$token;
 }
 
 function liveExecuteCommand($cmd, $err=false, $converter=null)
 {
-   
-	$lbr = "\n";
-	$lbr = "";
+    $lbr = "\n";
+    $lbr = "";
     while (@ ob_end_flush()); // end all output buffers if any
 
-    if($err){
+    if ($err) {
         $cmd.=" 2>&1";
     }
     // $proc = popen("$cmd 2>&1 ; echo Exit status : $?", 'r');
@@ -89,16 +112,15 @@ function liveExecuteCommand($cmd, $err=false, $converter=null)
     $live_output     = "";
     $complete_output = "";
 
-    while (!feof($proc))
-    {
+    while (!feof($proc)) {
         $live_output     = fread($proc, 4096);
-        if($converter){
+        if ($converter) {
             $live_output = $converter->convert($live_output);
         }
         $complete_output = $complete_output . $live_output;
         echo "$live_output".$lbr."<br>";
         // echo($converter->convert($live_output.$lbr)."<br>");
-       // echo json_encode(['txt'=>$live_output]);
+        // echo json_encode(['txt'=>$live_output]);
         @ flush();
     }
 
@@ -108,7 +130,7 @@ function liveExecuteCommand($cmd, $err=false, $converter=null)
     preg_match('/[0-9]+$/', $complete_output, $matches);
 
     // return exit status and intended output
-    return array (
+    return array(
                     'exit_status'  => intval($matches[0]),
                     'output'       => str_replace("Exit status : " . $matches[0], '', $complete_output)
                  );
