63011eeb2d79d2c9b2d2ab26fecd5999739fbce9
Robert Wagner <rw@20sec.net>
2022-04-21 22:22:11 +0200

    favicon

--- docs_src/src/layouts/default.php
+++ docs_src/src/layouts/default.php
@@ -9,7 +9,7 @@ $nav = ['items'=>[]];
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
-    <link rel="SHORTCUT ICON" href="/favicon.ico">
+    <link rel="SHORTCUT ICON" type="image/png" href="<?=path_asset('/gfx/favicon-96x96.png')?>">
 
     <link rel="stylesheet" href="<?=path_asset('/css/prism.css', true)?>" type="text/css">
     <link rel="stylesheet" href="<?=path_asset('/css/app.css', true)?>" type="text/css">
